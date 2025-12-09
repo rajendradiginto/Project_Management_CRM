5287a5395279a4b30aa08ea9d7238e4882293b19
dibyanshumehta <mehtadibyanshu2004@gmail.com>
2025-12-09 12:53:40 +0530

    updated Registration, App, Main files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-
+import Login from './components/auth/login.jsx'
+import Registration from './components/auth/Registration'
+import Dashboard from './components/dashboard/Dashboard.jsx'
 function App() {
 
   return (
     <>
-      
+    <Routes>
+      <Route path='/' element={<Registration/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
     </>
   )
 }
--- src/components/auth/Registration.jsx
+++ src/components/auth/Registration.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from "react";
 
 const Registration = () => {
   return (
-    <div>Registration</div>
-  )
-}
+    <>
+      <div className="container mt-5">
+        <div className="row justify-content-center text-center text-white">
+          <div className="col-lg-6 bg-dark">
+            <h2>Sign Up</h2>
+          </div>
+        </div>
+        <div className="row justify-content-center text-white">
+          <div className="col-lg-6 bg-dark py-2">
+            <form action="">
+              <div className="mb-3">
+                <label htmlFor="firstname" className="form-label">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="firstname"
+                  placeholder="Enter First Name"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="lastname" className="form-label">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lastname"
+                  placeholder="Enter Last Name"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="username" className="form-label">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  placeholder="Enter Username"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="profession" className="form-label">
+                  Profession
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="profession"
+                  placeholder="Enter Profession"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Enter Email"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Enter Password"
+                />
+              </div>
 
-export default Registration
+              <button className="btn btn-primary w-100">Sign Up</button>
+
+               <p>Already a user? <span>Login</span></p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Registration;
