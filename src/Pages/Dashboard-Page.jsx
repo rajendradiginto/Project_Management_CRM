3bc39c4e601770320a9093b1e40a0da9162b38b8
PAAGCDIGITAL123 <dwadeveloper123@gmail.com>
2025-12-13 10:32:52 +0530

    change the dashboard page and component

---
+++ src/Pages/Dashboard-Page.jsx
@@ -0,0 +1,68 @@
+import Dashboard from "../components/dashboard/Dashboard";
+
+const DashboardPage = () => {
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="row p-0">
+                    <div className="col-lg-2 p-0">
+                        <div className="card bg-light rounded-0 border-0">
+                            <ul className="list-group rounded-0">
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Dashboard</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Projects</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Pending Projects</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Dashboard</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Projects</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Pending Projects</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Dashboard</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Projects</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Pending Projects</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Dashboard</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Projects</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Pending Projects</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Dashboard</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Projects</b>
+                                </li>
+                                <li className="list-group-item list-group-item-action">
+                                    <b>Pending Projects</b>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg-10 bg-light">
+                        <Dashboard />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default DashboardPage;
