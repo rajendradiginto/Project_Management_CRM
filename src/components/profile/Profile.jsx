9677cf09083b7b7ab559c8fa5b47884acea5d35b
dibyanshumehta <mehtadibyanshu2004@gmail.com>
2026-01-10 10:51:23 +0530

    some changes on landing and profile page

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -28,7 +28,7 @@ const Profile = () => {
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-briefcase"
+                    className="bi bi-briefcase"
                     viewBox="0 0 16 16"
                   >
                     <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5m1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0M1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5" />
@@ -40,7 +40,7 @@ const Profile = () => {
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-diagram-2"
+                    className="bi bi-diagram-2"
                     viewBox="0 0 16 16"
                   >
                     <path
@@ -55,7 +55,7 @@ const Profile = () => {
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-building"
+                    className="bi bi-building"
                     viewBox="0 0 16 16"
                   >
                     <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z" />
@@ -68,7 +68,7 @@ const Profile = () => {
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-geo-alt"
+                    className="bi bi-geo-alt"
                     viewBox="0 0 16 16"
                   >
                     <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
@@ -87,7 +87,7 @@ const Profile = () => {
                     <h6>Task..</h6>
                     <p>Project Description</p>
 
-                    <h6>Project..</h6>  
+                    <h6>Project..</h6>
                     <p>Project Description</p>
                   </div>
                 </div>
@@ -102,7 +102,7 @@ const Profile = () => {
                     <p>Role Description</p>
                     <h6>Company..</h6>
                     <p>Role Description</p>
-                  </div>  
+                  </div>
                 </div>
               </div>
             </div>
